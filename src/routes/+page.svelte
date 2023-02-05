be7f917f3a9cee3ed148ef1d02d293b4f4ee89ae
mkit2009 <matejkotrba.skola@gmail.com>
2023-02-05 18:42:43 +0100

    Another commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,21 @@
+<script lang="ts">
+	let counter = 0;
+	let text = '';
+
+	function increment() {
+		counter += 1;
+	}
+</script>
+
 <h1>Home page</h1>
+
+<p>Counter: {counter}</p>
+<button on:click={increment}>Click</button>
+
+<input type="text" bind:value={text} />
+
+<p>Text: {text}</p>
+
+<button on:click={() => (text = '')}>Reset</button>
+
+<p>Some text</p>
